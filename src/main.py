c150e650fad390744567fc7dddd1e495b48b005c
Ryan Christopher <ryandchristopher27@gmail.com>
2022-09-25 22:50:43 -0500

    formatted main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 # main.py
 
+# Artificial Intelligence I
+# Homework 2
+
+# Ryan Christopher
+# Olivia Shouse
+# Luke Schaefer
+
 # Imports
 from iterativeDeepeningTree import *
 from uniformCostGraph import *
 from uniformCostTree import *
 
 # Uniform Cost Graph Search
-# Contains tests for both Instances
 UniformCostGraphSearchTests()
+
+# Unifor Cost Tree Search
 UniformCostTreeOutputs()
+
+# Iterative Deepening Search
 hw2IDS()
